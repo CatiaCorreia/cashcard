e2e964cb006495e4b23d696b046fac65e93fae70
CatiaCorreia <catia.correia97@gmail.com>
2025-09-26 17:10:56 +0100

    added functionality for gettign all cashcards in a sorted and paginated list

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,11 +1,15 @@
 package example.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -27,4 +31,25 @@ class CashCardController {
         }
         else return ResponseEntity.notFound().build();
     }
+
+    @GetMapping
+    private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                ));
+        return ResponseEntity.ok(page.getContent());
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb){
+        CashCard saveCashCard = cashCardRepository.save(newCashCardRequest);
+        URI locationOfNewCashcard = ucb
+                .path("cashcards/{id}")
+                .buildAndExpand(saveCashCard.id())
+                .toUri();
+        return ResponseEntity.created(locationOfNewCashcard).build();
+    };
 }
